40abdfb9dacf095524a3eb30aba7a952fec8ad54
Katerina <e.mayakovskaya@gmail.com>
2025-07-30 19:11:48 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,4 +1,5 @@
 import json
+import os
 import urllib.parse
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -6,36 +7,48 @@ from livereload import Server
 from more_itertools import chunked
 
 
-def render_page(template):
-    with open("meta_data.json", "r", encoding="utf-8") as file:
+def prepare_books_data(json_file_name):
+    """Получает .json с мета-данными о книгах, возвращает список словарей для рендера."""
+    with open(json_file_name, "r", encoding="utf-8") as file:
         books = json.load(file)
-
     for book in books:
-        # Кодируем путь к файлу в URL, но оставляем слэш нетронутым
         book['txt_url'] = urllib.parse.quote(book['book_path'], safe='/')
-
-    books_chunked = chunked(books, 2)
-
-    rendered_page = template.render(
-        books_chunked=books_chunked,
-    )
-
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
-    print("index.html updated")
+    return list(chunked(books, 2))
 
 
-def rebuild():
+def render_pages(books_chunked, path):
+    """Рендерит страницы на основе подготовленных данных."""
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml']),
     )
     template = env.get_template('template.html')
-    render_page(template)
+
+    pages = chunked(books_chunked, 5)
+
+    for index, content in enumerate(pages):
+        rendered_page = template.render(
+            content=content,
+        )
+        with open(f'{path}/index{index+1}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
+
+
+def rebuild():
+    path = 'pages'
+    os.makedirs(path, exist_ok=True)
+
+    json_file_name = 'meta_data.json'
+    books_chunked = prepare_books_data(json_file_name)
+
+    render_pages(books_chunked, path)
 
 
 if __name__ == '__main__':
     rebuild()
     server = Server()
-    server.watch('*.html', rebuild)
+    server.watch('template.html', rebuild)
+    server.watch('pages', rebuild)
+    server.watch('render_website.py', rebuild)
     server.serve(root='.')
+
